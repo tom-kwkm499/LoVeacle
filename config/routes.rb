63a67c7e251b9856b5326bbff29f9ba58c540ae3
tom-kwkm499 <kwkm.tomoo@gmail.com>
2025-04-27 11:51:12 +0000

    add view#p/registration,p/session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+
+
   devise_for :admins
-  devise_for :users
-  root to: "homes#top"
-  get "home/about", to: "homes#about"
+
+  devise_for :users,skip: [:passwords],  controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  namespace :admin do
+  end
+
+  scope module: :public do
+    root to: "homes#top"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

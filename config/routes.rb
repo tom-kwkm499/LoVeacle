99bd8dd01f14e0c4a38a759e2d8109cd03c8b8ad
tom-kwkm499 <kwkm.tomoo@gmail.com>
2025-04-27 17:41:48 +0000

    fix top remove about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
 
+  namespace :public do
+
+  end
+
   devise_for :admins
 
   devise_for :users,skip: [:passwords],  controllers: {
@@ -13,6 +17,9 @@ Rails.application.routes.draw do
 
   scope module: :public do
     root to: "homes#top"
+    get 'users/mypage'
+    get 'users/show'
+    get 'users/edit'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
